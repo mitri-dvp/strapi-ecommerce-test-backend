53951b00186d24e2afc1aca77551e002b6a07c95
Jorge Mitri <mitri.dvp@gmail.com>
2020-12-31 00:24:25 -0300

    URL value fix

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'http://localhost:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
