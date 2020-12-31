a024fe9a75c55116677e7f881d610d04807538e8
Jorge Mitri <mitri.dvp@gmail.com>
2020-12-31 14:51:50 -0300

    Heroku URL added

--- config/server.js
+++ config/server.js
@@ -1,9 +1,26 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === 'production') {
+    return {
+      host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+      url: env('HEROKU_URL'),
+      admin: {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET'),
+        },
+      },
+    };
+  }
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET'),
+      },
     },
-  },
-});
+  };
+
+};
